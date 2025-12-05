47bf78e59dbce45f69fcd57c692529cd731021f2
zesube <zawadiesube23@gmail.com>
2025-12-05 14:07:59 -0500

    Restore public/db.js to last committed version

--- public/db.js
+++ public/db.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://group4_db:Password4@group4songo.t5wsoob.mongodb.net/?appName=Group4Songo", 
+mongoose.connect("mongodb+srv://group4_db:Password4@group4songo.t5wsoob.mongodb.net/?appName=Group4Songo",
 {useNewUrlParser: true})
+
+module.exports = mongoose;
+const mongoose = require('mongoose')
+
+// Connect without deprecated/unsupported options
+mongoose.connect(
+	"mongodb+srv://group4_db:Password4@group4songo.t5wsoob.mongodb.net/?appName=Group4Songo"
+)
+	.then(() => console.log('MongoDB connected'))
+	.catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose;
